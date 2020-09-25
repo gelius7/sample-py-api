985868b3ecfeadff07b18cf103039a9cfaff2f3d
jamje <gelius7@gmail.com>
2020-09-26 00:44:53 +0900

    update Dockerfile, add GET,POST

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@ FROM python:3.7
 
 MAINTAINER gelius "gelius7@gmail.com"
 
-#RUN pip3 install flask
 COPY . /app
 WORKDIR /app
 
@@ -10,14 +9,6 @@ RUN pip3 install -r requirements.txt
 
 EXPOSE 80
 
-CMD ["app.py"]
+ENTRYPOINT [ "python3" ]
 
-#FROM ubuntu:latest
-#MAINTAINER gelius "gelius7@gmail.com"
-#RUN apt-get update -y
-#RUN apt-get install -y python-pip python-dev build-essential
-#COPY ./app.py /app
-#WORKDIR /app
-#RUN pip install -r requirements.txt
-#ENTRYPOINT ["python"]
-#CMD ["app.py"]
+CMD ["app.py"]
--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, request, escape
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/', methods=['GET'])
+def hello_get():
+    uid = request.args.get('uid')
+    return f'Hello {escape(uid)}! by get'
+
+
+@app.route('/', methods=['POST'])
+def hello_post():
+    if request.is_json is True:  # Content-Type: json
+        print('json type in')
+        content = request.get_json()
+    else:  # Content-Type: x-www-form-urlencoded
+        print('urlencoded type in')
+        content = request.form
+    uid = content['uid']
+    return f'Hello, {escape(uid)}! by post'
 
 
 if __name__ == '__main__':
