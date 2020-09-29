424ed34ad13554d2a21d557b4ba9685e8de81c80
jamje <gelius7@gmail.com>
2020-09-29 21:27:31 +0900

    latest push

--- ci-docker.sh
+++ ci-docker.sh
@@ -91,13 +91,13 @@ _docker() {
     _command "docker push ${USERNAME}/${REPONAME}:${VERSION}"
     docker push ${USERNAME}/${REPONAME}:${VERSION}
 
-    if [ "${PARAM}" == "latest" ]; then
-        _command "sudo docker tag ${USERNAME}/${REPONAME}:${VERSION} ${USERNAME}/${REPONAME}:latest"
-        sudo docker tag ${USERNAME}/${REPONAME}:${VERSION} ${USERNAME}/${REPONAME}:latest
+#    if [ "${PARAM}" == "latest" ]; then
+    _command "docker tag ${USERNAME}/${REPONAME}:${VERSION} ${USERNAME}/${REPONAME}:latest"
+    docker tag ${USERNAME}/${REPONAME}:${VERSION} ${USERNAME}/${REPONAME}:latest
 
-        _command "docker push ${USERNAME}/${REPONAME}:latest"
-        docker push ${USERNAME}/${REPONAME}:latest
-    fi
+    _command "docker push ${USERNAME}/${REPONAME}:latest"
+    docker push ${USERNAME}/${REPONAME}:latest
+#    fi
 
     _command "docker logout"
     docker logout
